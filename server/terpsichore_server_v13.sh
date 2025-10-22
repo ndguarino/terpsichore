25151d1d235abfc03ebb834395a15f4f968ff017
ndguarino <me@ndguarino.com>
2025-10-22 13:32:02 -0400

    Add docker patchfile

---
+++ server/terpsichore_server_v13.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ -e /data/Data/modules/terpsichore/server/terpsichore_server_v13.mjs ]]; then
+  log "Applying Terpsichore patch..."
+  cp /data/Data/modules/terpsichore/server/terpsichore_server_v13.mjs $HOME/resources/app
+  sed -i 's$  init.default$  await init.default$' $HOME/resources/app/main.mjs
+  sed -i 's$  \});$  });\n  await (await import("./terpsichore_server_v13.mjs")).Terpsichore.init();$' $HOME/resources/app/main.mjs
+else
+  log "Unable to find Terpsichore patch."
+fi
