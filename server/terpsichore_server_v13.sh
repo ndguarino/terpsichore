4ccb63cede1d6532798f7499ff1220aed9f5b996
ndguarino <me@ndguarino.com>
2025-10-22 14:25:47 -0400

    V12 compat, v0.1.1

--- server/terpsichore_server_v13.sh
+++ server/terpsichore_server_v13.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+RESOURCE_HOME="${FOUNDRY_HOME:-$HOME}"
+log "Resource home: $RESOURCE_HOME"
+
 if [[ -e /data/Data/modules/terpsichore/server/terpsichore_server_v13.mjs ]]; then
   log "Applying Terpsichore patch..."
-  cp /data/Data/modules/terpsichore/server/terpsichore_server_v13.mjs $HOME/resources/app
-  sed -i 's$  init.default$  await init.default$' $HOME/resources/app/main.mjs
-  sed -i 's$  \});$  });\n  await (await import("./terpsichore_server_v13.mjs")).Terpsichore.init();$' $HOME/resources/app/main.mjs
+  cp /data/Data/modules/terpsichore/server/terpsichore_server_v13.mjs $RESOURCE_HOME/resources/app
+  sed -i 's$  init.default$  await init.default$' $RESOURCE_HOME/resources/app/main.mjs
+  sed -i 's$  \});$  });\n  await (await import("./terpsichore_server_v13.mjs")).Terpsichore.init();$' $RESOURCE_HOME/resources/app/main.mjs
 else
   log "Unable to find Terpsichore patch."
 fi
